701fc8c642f6d5abe70221996d84c0dc1591f7f3
AtnibamAitay <atnibamaitay@foxmail.com>
2023-12-13 09:41:01 +0800

    refactor(用户界面): 简化退出登录功能和清理本地存储逻辑
    - 将退出登录时清除的localStorage的多个条目改为一次性清空，使用localStorage.clear()替代移除单独的'accessToken'和'permission'。
    - 移除了样式中未使用的注释代码及不必要的注释，使代码更加简洁。
    
    这些更改旨在提高代码的可维护性和清晰度，同时确保用户退出登录时能够彻底清除所有的本地会话信息。

--- src/components/common/TabControl.vue
+++ src/components/common/TabControl.vue
@@ -1,11 +1,8 @@
 <template>
-  <!-- 创建一个背景容器 -->
   <div class="card">
-    <!-- 创建设置按钮 -->
     <button class="btn btn-top" @mouseover="hover = true" @mouseleave="hover = false">
       设置
     </button>
-    <!-- 创建退出登录按钮 -->
     <button class="btn btn-bottom" @click="logout" @mouseover="hover = true" @mouseleave="hover = false">
       退出登录
     </button>
@@ -14,24 +11,20 @@
 
 <script setup lang="js">
 import {ref} from 'vue'
-import {useRouter} from 'vue-router' // 引入路由对象
+import {useRouter} from 'vue-router'
 import axios from 'axios'
 
-
-// 定义鼠标悬停状态
 const hover = ref(false)
 
-// 获取路由对象实例
 const router = useRouter()
 const showButtons = ref(false);
 const apiBaseUrl = import.meta.env.VITE_API_LOCALHOST_BASE_PATH;
 
-// 定义退出登录函数
+// 退出登录函数
 const logout = async () => {
   try {
     await axios.post(`${apiBaseUrl}/user/logout`);
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('permission');
+    localStorage.clear();
     router.push('/acacc/login');
   } catch (error) {
     console.error('Logout failed:', error);
@@ -41,9 +34,6 @@ const logout = async () => {
 
 <style scoped>
 .card {
-  /* 将px单位转换为rem单位 */
-  /* width: 13.875rem;
-  height: 6.6875rem; */
   background-color: #ffffff;
   border-radius: 1.25rem;
   box-shadow: 0 0 0.3125rem rgba(113, 113, 113, 0.35);
@@ -77,12 +67,7 @@ const logout = async () => {
   border-top-left-radius: 1.25rem;
   border-bottom-right-radius: 0;
   border-bottom-left-radius: 0;
-  /* border-bottom: 1px solid #e0e0e0; */
-  /* margin-bottom: 0.1rem; */
-  /* height: 2.8rem;
-  width: 8rem; */
   position: relative;
-  /* top: 0.2rem; */
 }
 
 .btn-bottom {
@@ -91,6 +76,5 @@ const logout = async () => {
   border-bottom-right-radius: 1.25rem;
   border-bottom-left-radius: 1.25rem;
   margin-top: 0.1875rem;
-
 }
 </style>
